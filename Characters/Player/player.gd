45c89fd29ea63b446cd28a795fbf61fb03cc509f
Mainuddin Khaled <mainuddinshahar1393@gmail.com>
2026-09-06 23:00:25 +0600

    Improved Aiming and Smoothing
    Smooth camera positioning and aiming

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -5,7 +5,7 @@ const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 
 var mouse_motion := Vector2.ZERO
-
+@onready var camera_pivot: Node3D = $CameraPivot
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
@@ -42,4 +42,8 @@ func _input(event: InputEvent) -> void:
 
 func handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
+	camera_pivot.rotate_x(mouse_motion.y)
+	camera_pivot.rotation_degrees.x = clampf(
+		camera_pivot.rotation_degrees.x, -90, 90
+	)
 	mouse_motion = Vector2.ZERO
